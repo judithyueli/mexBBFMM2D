1fc9d59d15931e71466eca002aedf5d5f3fe15b2
Yue Li <yuel@stanford.edu>
2015-11-17 19:45:11 -0800

    update callmxFMM2D

--- callmxFMM2D.m
+++ callmxFMM2D.m
@@ -11,7 +11,7 @@ y = linspace(0,1,Ny);
 xloc = xloc(:);  yloc = yloc(:);
 
 
- 
+%% testing mode 
 H = ones(N,100);    % right muliplier H, each column is a Nx1 vector
 nCheb = 6;          % number of Chebyshev node
 print = true;       % print exact computation time and accuracy
@@ -19,3 +19,5 @@ print = true;       % print exact computation time and accuracy
 % Compute matrix-matrix product QH of dimension 10000x100
 [QH,QHexact] = case1(xloc, yloc,H,nCheb,print);
 
+%% application mode
+QH = case1(xloc, yloc,H,nCheb,print);
