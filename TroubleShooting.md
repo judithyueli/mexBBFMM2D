e3a78000bcacac42e0d301bbf4a578c8fb93a416
Yue Li <yuel@stanford.edu>
2015-11-18 17:26:27 -0800

    Add troubleshooting page

---
+++ TroubleShooting.md
@@ -0,0 +1,30 @@
+##Trouble shooting for  mexBBFMM2D
+==========
+
+- How do I find the correct C++ compiler?
+
+See [here](http://www.mathworks.com/support/sysreq/previous_releases.html)
+
+- __For Mac:__If you get an error saying that SDK files are missing, e.g.,  
+```
+Looking for folder '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk' ...No. 
+```
+you can find the sdk files here https://github.com/phracker/MacOSX-SDKs/releases. Download those that are missing and place them in the appropriate directories based on the error message you get. 
+
+- __For PC:__
+```
+Building with 'Microsoft Windows SDK 7.1 (C++)'.
+Error using mex
+mexFMM2D.cpp
+c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\INCLUDE\intrin.h(26) : fatal error C1083: Cannot open
+include file: 'ammintrin.h': No such file or directory
+ 
+Error in make (line 38)
+mex('-O','mexFMM2D.cpp',src1,src2,src3,'-largeArrayDims',['-I',eigenDIR],['-I',fmmDIR],'-output',filename)
+ 
+Error in compilemex (line 5)
+make(r,kernel,outputfile);
+```
+See [here](http://www.mathworks.com/matlabcentral/answers/90383-fix-problem-when-mex-cpp-file)
+
+
--- callmxFMM2D.m
+++ callmxFMM2D.m
@@ -1,4 +1,4 @@
-QH = callmxFMM2D(ExecFile,TestingMode)
+function QH = callmxFMM2D(ExecFile,TestingMode)
 
 %% This is an example for using mexBBFMM2D to multiply the covariance matrix for a regular 100 x 100 grid, with matrix H. 
 
@@ -10,7 +10,7 @@ QH = callmxFMM2D(ExecFile,TestingMode)
 % Grid information - regular 100x100 grid in [0,1]
 % xloc, yloc: These are the coordinates of each grid point provided as vectors. 
 %             These are m x 1 vectors arranged in a consistent order. 
-%             For example for a 2x2 grid in [0,1] it would be xloc=[0 0 1 1]’ and yloc=[0 1 0 1]’. 
+%             For example for a 2x2 grid in [0,1] it would be xloc=[0 0 1 1]' and yloc=[0 1 0 1]'. 
 %             The grid does not have to be regular or structured.
 
 % Modify xloc and yloc for your own example
@@ -30,9 +30,10 @@ PrintFlag = true;   % print exact computation time and accuracy
 if TestingMode
   %% testing mode 
   % Compute matrix-matrix product QH of dimension 10000x100
-  eval('[QH,QHexact] = ',ExecFile,'(xloc, yloc,H,nCheb,PrintFlag);');
+  eval(['[QH,QHexact] = ',ExecFile,'(xloc, yloc,H,nCheb,PrintFlag);']);
 else
   %% application mode
-  eval('QH = ',ExecFile,'(xloc, yloc,H,nCheb,PrintFlag);');
+  eval(['QH = ',ExecFile,'(xloc, yloc,H,nCheb,PrintFlag);']);
 end
-  
+
+end
